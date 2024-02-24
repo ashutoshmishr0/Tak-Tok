487b5362722bf850d8f96f859912cc0770707d22
ashutoshmishr0 <122212306+ashutoshmishr0@users.noreply.github.com>
2024-02-24 10:32:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ app.use(errorHandler);
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "productions") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
