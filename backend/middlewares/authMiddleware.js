255fe2a54ec2c7b6065687b068df3195cea11759
ashutoshmishr0 <122212306+ashutoshmishr0@users.noreply.github.com>
2024-02-24 11:21:11 +0530

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt =require("jsonwebtoken");
-const User=require("../Models/userModel.js");
+const User=require("../models/userModel");
 const asyncHandler=require("express-async-handler");
 
 
